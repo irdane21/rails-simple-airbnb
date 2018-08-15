54a8d03a0ba6d8b9772521c3e547426dab1f3092
Adrien Lefrancq <irdane2111@gmail.com>
2018-08-15 17:52:49 +0200

    second push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'duplex in New York',
+  address: '111 rue de la liberté',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 335,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'duplex in Paris',
+  address: '111 rue de la liberté',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 115,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'duplex in London',
+  address: '111 rue de la liberté',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 225,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'duplex in Dubai',
+  address: '111 rue de la liberté',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 375,
+  number_of_guests: 8
+)
